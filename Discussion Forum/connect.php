a863e04745ac4cfec0346e7362efc5d676adfb6d
tarkowr <34112306+tarkowr@users.noreply.github.com>
2018-07-02 18:40:06 -0400

    Update connect.php

--- Discussion Forum/connect.php
+++ Discussion Forum/connect.php
@@ -4,11 +4,7 @@ function doDB() {
 
 	//
 	// connect to server and select database
-	// Uncomment the correct connection string for use
 	//
-
-	//$mysqli = mysqli_connect("localhost:3306", "lisabalbach_tarkowr", "CIT1802511", "lisabalbach_testdb");
-	//$mysqli = mysqli_connect("localhost:3306", "lisabalbach_scooby", "CIT1995*", "lisabalbach_testdb");
 	$mysqli = mysqli_connect("localhost", "root", "", "MarchMadness");
 
 	//if connection fails, stop script execution
